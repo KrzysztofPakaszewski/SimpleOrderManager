653cc81ac8533dd547a21330415a922abfeada9c
KrzysztofPakaszewski <krzysztof.pakaszewski@gmail.com>
2020-07-21 16:08:06 +0200

    Init

---
+++ src/main/java/example/SimpleOrderManager/SimpleOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package example.SimpleOrderManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOrderManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/SimpleOrderManager/SimpleOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package example.SimpleOrderManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
